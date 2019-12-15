a880ab11a96e02e8669ff2950938892b9d155de9
EC2 Default User <ec2-user@ip-172-31-16-85.us-east-2.compute.internal>
2019-12-15 21:14:10 +0000

    a;sldkfj

--- StockMarketPredictions/db_job.py
+++ StockMarketPredictions/db_job.py
@@ -5,12 +5,6 @@ import sys
 os.environ['NO_PROXY'] = '127.0.0.1'
 
 db_key = sys.argv[1]
-with open("secrets/secrets.py") as f:
-	for line in f:
-		key, val = line.split('=')
-		if key.strip() == 'DB_KEY':
-			db_key = val[2:len(val)-1]
-
 
 r = requests.get('http://127.0.0.1/get-next-day/' + db_key)
 print(r.json())
